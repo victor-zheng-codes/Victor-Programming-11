3a7b20a160745c2f424229ef197f5a7444c3dad3
vic-programer <52824986+vic-programer@users.noreply.github.com>
2020-08-02 09:38:04 -0700

    Final Commit for ProjectTwoVideoDemo

--- ProjectTwoVideoDemonstration/src/sample/Controller.java
+++ ProjectTwoVideoDemonstration/src/sample/Controller.java
@@ -1,4 +1,44 @@
+//Project was to test how to implement a video on JavaFX
+//Took a while to get used to stages and scenes. Took around 10 hrs of learning for me to really understand what was going on
+// Started July 27, Ended August 2
+// Victor Zheng was the sole developer. All work was made by me, and is not intended for the use of others.
 package sample;
 
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class Controller {
+    public VBox mediaArea;
+
+
+    public void playVideo(MouseEvent mouseEvent) {
+        Stage newStage = new Stage();
+        // Create the media source.
+        //String source = getParameters().getRaw().get(0);
+        File f = new File("C:\\Users\\zheng\\IdeaProjects\\ProjectTwoVideoDemonstration\\src\\sample\\Garden_Video.mp4");
+        Media m = new Media(f.toURI().toString());
+
+        // Create the player and set to play automatically.
+        MediaPlayer mediaPlayer = new MediaPlayer(m);
+        mediaPlayer.setAutoPlay(true);
+
+        // Create the view and add it to the Scene.
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+        //stage.setScene(new Scene(root, 680, 400));
+        mediaArea = new VBox(mediaView);
+        Scene scene = new Scene(mediaArea, 640, 500);
+
+        newStage.setScene(scene);
+        newStage.setTitle("Demonstration Video");
+        newStage.show();
+        mediaPlayer.play();
+    }
 }
--- ProjectTwoVideoDemonstration/src/sample/Main.java
+++ ProjectTwoVideoDemonstration/src/sample/Main.java
@@ -2,14 +2,9 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -19,51 +14,11 @@ public class Main extends Application {
     public VBox mediaArea;
 
     @Override
-    /*
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
 
-
-
-    public void start(Stage stage) throws Exception{
-        // Create and set the Scene.
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        Scene scene = new Scene(root, 640, 500);
-
-        //Scene scene = new Scene(new Group(), 640, 500);
-        stage.setScene(scene);
-
-        // Name and display the Stage.
-        stage.setTitle("Hello Media");
-        stage.show();
-
-        // Create the media source.
-        //String source = getParameters().getRaw().get(0);
-        File f = new File("C:\\Users\\zheng\\IdeaProjects\\ProjectTwoVideoDemonstration\\src\\sample\\Garden_Video.mp4");
-        Media m = new Media(f.toURI().toString());
-
-        // Create the player and set to play automatically.
-        MediaPlayer mediaPlayer = new MediaPlayer(m);
-        mediaPlayer.setAutoPlay(true);
-
-        // Create the view and add it to the Scene.
-        MediaView mediaView = new MediaView(mediaPlayer);
-        //((Group) scene.getRoot()).getChildren().add(mediaView);
-        //stage.setScene(new Scene(root, 680, 400));
-        mediaPlayer.play();
-        stage.show();
-    }
-
-     */
     public void start(Stage primaryStage) throws Exception{
         // Create and set the Scene.
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Scene scene = new Scene(root, 640, 500);
+        Scene scene = new Scene(root, 700, 600);
 
         //Scene scene = new Scene(new Group(), 640, 500);
         primaryStage.setScene(scene);
@@ -74,34 +29,5 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    public void playVideo(MouseEvent mouseEvent) {
-        Stage newStage = new Stage();
-        // Create the media source.
-        //String source = getParameters().getRaw().get(0);
-        File f = new File("C:\\Users\\zheng\\IdeaProjects\\ProjectTwoVideoDemonstration\\src\\sample\\Garden_Video.mp4");
-        Media m = new Media(f.toURI().toString());
-
-        // Create the player and set to play automatically.
-        MediaPlayer mediaPlayer = new MediaPlayer(m);
-        mediaPlayer.setAutoPlay(true);
-
-        // Create the view and add it to the Scene.
-        MediaView mediaView = new MediaView(mediaPlayer);
-        //((Group) scene.getRoot()).getChildren().add(mediaView);
-        //stage.setScene(new Scene(root, 680, 400));
-        mediaArea = new VBox(mediaView);
-        mediaArea.setDisable(false);
-        mediaArea.setVisible(true);
-
-        Scene scene = new Scene(mediaArea, 640, 500);
-
-        //Scene scene = new Scene(new Group(), 640, 500);
-        newStage.setScene(scene);
-        newStage.setTitle("Demonstration Video");
-
-        newStage.show();
-        mediaPlayer.play();
     }
 }
